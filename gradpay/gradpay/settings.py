5bad3e7999f729f733b2181feb2acd4b29e92dc8
jmcarp <jmcarp@mac-tpolk13.psych.lsa.umich.edu>
2013-01-22 12:21:34 -0500

    static

--- gradpay/gradpay/settings.py
+++ gradpay/gradpay/settings.py
@@ -9,6 +9,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+import os.path
+PROJECT_ROOT = os.path.dirname(__file__)
+
 LOGIN_REDIRECT_URL = '/'
 CRISPY_TEMPLATE_PACK = 'bootstrap'
 
@@ -59,7 +62,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'gradpay'
+#STATIC_ROOT = 'gradpay'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -105,11 +109,8 @@ ROOT_URLCONF = 'gradpay.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'gradpay.wsgi.application'
 
-import os.path
-PROJECT_DIR = os.path.dirname(__file__)
-
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_DIR, '../templates'),
+    os.path.join(PROJECT_ROOT, '../templates'),
     '/Users/jmcarp/Dropbox/projects/gradpay/gradpay/templates',
 )
 
