fdbab34075b5e02b2a0623588b95df7277fdcfda
jmcarp <jmcarp@mac-tpolk13.psych.lsa.umich.edu>
2013-02-23 14:05:13 -0500

    rewrote table sort (test)

--- gradpay/gradpay/views.py
+++ gradpay/gradpay/views.py
@@ -1,7 +1,6 @@
 # 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.contrib.auth.decorators import login_required
 
 # Import models
 from models import Survey
@@ -36,27 +35,82 @@ def fmt_factory(pct, rnd):
     return '%s%s' % (num, app)
   return fmt
 
-def row_extract(row, var):
-  if isinstance(var, tuple):
-    return var[1](row[var[0]])
-  return row[var]
+#def row_extract(row, var):
+#  if 'fun' in var:
+#    return var['fun'](row[var['name']])
+#  return row[var['name']]
+#def row_extract(row, var):
+#  if isinstance(var, tuple):
+#    return var[1](row[var[0]])
+#  return row[var]
 
 def var_name(var):
   if isinstance(var, tuple):
     return var[0]
   return var
 
-stored_vars = [
-  'institution__name',
-  'institution__state',
-  'department__name',
-]
-computed_vars = [
-  ('avg_stipend', fmt_factory(False, 0)),
-  ('avg_teach_frac', fmt_factory(True, 0)),
-  'num_resp',
-]
-vars = stored_vars + computed_vars
+class VarInfo(object):
+  
+  def __init__(self, name, type, fun=None):
+    self.name = name
+    self.type = type
+    self.fun = fun
+
+  def extract(self, row):
+    val = row[self.name]
+    if self.fun is not None:
+      return self.fun(val)
+    return val
+
+vars = {
+  'institution' : VarInfo('institution__name', 'stored'),
+  'state' : VarInfo('institution__state', 'stored'),
+  'department' : VarInfo('department__name', 'stored'),
+  'avg_stipend' : VarInfo('avg_stipend', 'computed', fmt_factory(False, 0)),
+  'avg_teach_frac' : VarInfo('avg_teach_frac', 'computed', fmt_factory(True, 0)),
+  'num_resp' : VarInfo('num_resp', 'computed'),
+}
+"""
+vars = {
+  'institution' : {
+    'name' : 'institution__name',
+    'type' : 'stored',
+  },
+  'state' : {
+    'name' : 'institution__state',
+    'type' : 'stored',
+  },
+  'department' : {
+    'name' : 'department__name',
+    'type' : 'stored',
+  },
+  'avg_stipend' : {
+    'name' : 'avg_stipend',
+    'type' : 'computed',
+    'fun' : fmt_factory(False, 0),
+  },
+  'avg_teach_frac' : {
+    'name' : 'avg_teach_frac',
+    'type' : 'computed',
+    'fun' : fmt_factory(True, 0),
+  },
+  'num_resp' : {
+    'name' : 'num_resp',
+    'type' : 'computed',
+  },
+}
+"""
+#stored_vars = [
+#  'institution__name',
+#  'institution__state',
+#  'department__name',
+#]
+#computed_vars = [
+#  ('avg_stipend', fmt_factory(False, 0)),
+#  ('avg_teach_frac', fmt_factory(True, 0)),
+#  'num_resp',
+#]
+#vars = stored_vars + computed_vars
 
 sort_map = {
   'asc' : '',
@@ -74,12 +128,14 @@ def results_json(request):
   sEcho = request.GET.get('sEcho', 0)
 
   # Get search units
-  units = request.GET.get('units', 'institution')
-  units = units.split(',')
-  units = [relation_map[unit] for unit in units if unit in relation_map]
+  grouping_variables = request.GET.get('grouping_vars', 'institution')
+  grouping_variables = grouping_variables.split(',')
+  #units = request.GET.get('units', 'institution')
+  #units = units.split(',')
+  #units = [relation_map[unit] for unit in units if unit in relation_map]
   
   # Get display variables
-  display_variables = request.GET.get('display', 'salary')
+  display_variables = request.GET.get('display_vars', 'salary')
   display_variables = display_variables.split(',')
   if not display_variables[0]:
     display_variables = [] 
@@ -87,26 +143,47 @@ def results_json(request):
   annotate_args = {}
   for dv in display_variables:
     annotate_args[dv] = annotate_map[dv]
+  
+  columns = grouping_variables + display_variables
 
   # Get search term
   like = request.GET.get('sSearch', '')
   like_lookup = ''
   if like:
-    like_lookup = Q(**{stored_vars[0] + '__icontains' : like})
-    for var in stored_vars[1:]:
+    visible_stored_vars = [col for col in columns if vars[col]['type'] == 'stored']
+    like_lookup = Q(**{visible_stored_vars[0] + '__icontains' : like})
+    for var in visible_stored_vars[1:]:
       like_lookup = like_lookup | Q(**{var + '__icontains' : like})
+    #like_lookup = Q(**{stored_vars[0] + '__icontains' : like})
+    #for var in stored_vars[1:]:
+    #  like_lookup = like_lookup | Q(**{var + '__icontains' : like})
+  
+  # Sorting
+  order_by_fields = []
+  n_sort_cols = request.GET.get('iSortingCols')
+  n_sort_cols = int(n_sort_cols)
+  for sort_idx in range(n_sort_cols):
+    sort_pos = request.GET.get('iSortCol_%d' % (sort_idx))
+    sort_pos = int(sort_pos)
+    sort_var = vars[columns[sort_pos]]
+    sort_name = sort_var.name
+    #sort_name = sort_var['name']
+    sort_dir = request.GET.get('sSortDir_%d' % (sort_idx))
+    sort_sign = sort_map[sort_dir]
+    order_by_fields.append('%s%s' % (sort_sign, sort_name))
 
-  # Get sort variable
-  sort_col = request.GET.get('iSortCol_0', '0')
-  sort_col = int(sort_col)
-  sort_var = vars[sort_col]
-  if isinstance(sort_var, tuple):
-    sort_var = sort_var[0]
+  ## Get sort variable
+  #sort_col = request.GET.get('iSortCol_0', '0')
+  #sort_col = int(sort_col)
+  #sort_var = vars[columns[sort_col]]['name']#columns[sort_col]['name']
+  ##sort_var = vars[sort_col]
+  ##if isinstance(sort_var, tuple):
+  ##  sort_var = sort_var[0]
 
-  # Get sort direction
-  sort_dir = request.GET.get('sSortDir_0', 'dsc')
-  sort_sign = sort_map[sort_dir]
-  order_by = sort_sign + sort_var
+  ## Get sort direction
+  #sort_dir = request.GET.get('sSortDir_0', 'dsc')
+  #sort_sign = sort_map[sort_dir]
+  #order_by = sort_sign + sort_var
 
   # Get offset and limit
   offset = request.GET.get('iDisplayStart', 0)
@@ -125,10 +202,13 @@ def results_json(request):
     rows = rows.filter(like_lookup)
 
   # Compute average stipend
-  rows = rows.values(*units).annotate(**annotate_args)
+  rows = rows.values(*[vars[g].name for g in grouping_variables]).annotate(**annotate_args)
+  #rows = rows.values(*[vars[g]['name'] for g in grouping_variables]).annotate(**annotate_args)
+  #rows = rows.values(*units).annotate(**annotate_args)
   
   # Order
-  rows = rows.order_by(order_by)
+  rows = rows.order_by(*order_by_fields)
+  #rows = rows.order_by(order_by)
 
   # Get total count
   count_total = rows.count()
@@ -138,7 +218,9 @@ def results_json(request):
 
   aaData = []
   for row in rows:
-    aaData.append([row_extract(row, var) for var in vars if var_name(var) in row])
+    aaData.append([vars[col].extract(row) for col in columns if vars[col].name in row])
+    #aaData.append([row_extract(row, vars[col]) for col in columns if vars[col]['name'] in row])
+    #aaData.append([row_extract(row, var) for var in vars if var_name(var) in row])
 
   # Assemble data
   data = {
