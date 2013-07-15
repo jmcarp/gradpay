cc001c9d4f14b7636c8d7ae8af98ae4ac6cf8118
jmcarp <jm.carp@gmail.com>
2013-07-14 21:51:37 -0400

    typo

--- gradpay/gradpay/views.py
+++ gradpay/gradpay/views.py
@@ -143,7 +143,7 @@ def scatter_json(request):
         result = {column : vars[column].extract(row)
             for column in columns
             if vars[column].name in row}
-        result['_label'] = ' : '.join([result[group] for group in grouping_vars])
+        result['_label'] = ' : '.join([result[g] for g in grouping_variables])
         results.append(result)
 
     # Serialize data to JSON
